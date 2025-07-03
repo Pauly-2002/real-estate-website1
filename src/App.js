baaba56e43c1a1f31e2ca3976727c3c6c0a43c43
Oluwasegun_Paul <adewuyiogsa2019@gmail.com>
2025-07-03 19:17:43 +0100

    fix: corrected component import paths

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import './index.css';
+import "./index.css";
 import Home from "./components/Home";
 import FeaturedProperty from "./components/FeaturedProperty";
 import PropertyModel from "./components/PropertyModel";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
-
-  const [selectedProperty, setSelectedProperty]= useState(null);
+  const [selectedProperty, setSelectedProperty] = useState(null);
 
   return (
     <div className="App">
-     <Home/>
+      <Home />
 
-     <FeaturedProperty setSelectedProperty={setSelectedProperty} />
-     {
-      selectedProperty&& (
-        <PropertyModel properties={[selectedProperty]} onClose={()=> setSelectedProperty(null)} />
-      )
-     }
-     <Contact />
-     <Footer />
+      <FeaturedProperty setSelectedProperty={setSelectedProperty} />
+      {selectedProperty && (
+        <PropertyModel
+          properties={[selectedProperty]}
+          onClose={() => setSelectedProperty(null)}
+        />
+      )}
+      <Contact />
+      <Footer />
     </div>
   );
 }
